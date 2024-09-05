53705a564a30724548a8a93ae456a5a0d0391f9b
Munif Gebara Junior <munifgebara@gmail.com>
2024-09-05 15:22:03 -0300

    first commit

---
+++ lbp_example.py
@@ -0,0 +1,121 @@
+import os
+import numpy as np
+import cv2
+from skimage.feature import local_binary_pattern
+from sklearn.model_selection import GridSearchCV, train_test_split
+from sklearn.svm import SVC
+from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neighbors import KNeighborsClassifier
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+# LBP
+class LocalBinaryPatterns:
+    def __init__(self, numPoints, radius):
+        self.numPoints = numPoints
+        self.radius = radius
+
+    def describe(self, image, eps=1e-7):
+        lbp = local_binary_pattern(image, self.numPoints, self.radius, method="nri_uniform")
+        n_bins = int(lbp.max() + 1)
+        hist, _ = np.histogram(lbp.ravel(), bins=n_bins, range=(0, n_bins))
+
+        hist = hist.astype("float")
+        hist /= (hist.sum() + eps)
+        return hist
+
+
+# Configurações do LBP
+feature_extractor = LocalBinaryPatterns(8, 2)
+
+images = []
+labels = []
+textLabels = []
+
+baseFolder = 'nexus_encrypted/'
+
+folders = os.listdir(baseFolder)
+l = 0
+for folder in folders:
+    print('Loading folder ', folder)
+    c = 0
+
+    if len(os.listdir(baseFolder + folder))<100 :
+        print ("não usando ",folder," por ter somente ", len(os.listdir(baseFolder + folder)), "amostras")
+        continue
+    textLabels.append(folder)
+    for image_path in os.listdir(baseFolder + folder):
+        image = cv2.imread(os.path.join(baseFolder + folder, image_path), cv2.IMREAD_GRAYSCALE)
+        # image = cv2.resize(image, (256, 256))
+        images.append(image)
+        labels.append(l)
+
+        c += 1
+        if c >= 100:
+            break
+    l += 1
+
+t=0
+for textLabel in textLabels:
+    print (t,textLabel)
+    t+=1
+
+# Extração das características LBP
+data = []
+for image in images:
+    hist = feature_extractor.describe(image)
+    data.append(hist)
+
+data = np.array(data)
+labels = np.array(labels)
+
+# Divisão do dataset
+X_train, X_test, y_train, y_test = train_test_split(data, labels, test_size=0.2, random_state=180875)
+
+# SVM com GridSearchCV
+param_grid_svm = {'C': [1000, 2000, 10000], 'gamma': [2,1, 0.1, 0.01, 0.001, 0.0001], 'kernel': ['rbf']}
+grid_svm = GridSearchCV(SVC(), param_grid_svm, refit=True, verbose=3)
+grid_svm.fit(X_train, y_train)
+
+print("Best parameters for SVM: ", grid_svm.best_params_)
+print("Best estimator for SVM: ", grid_svm.best_estimator_)
+
+# RandomForest com GridSearchCV
+param_grid_rf = {'n_estimators': [10, 50, 100, 200], 'max_depth': [None, 10, 20, 30]}
+grid_rf = GridSearchCV(RandomForestClassifier(), param_grid_rf, refit=True, verbose=3)
+grid_rf.fit(X_train, y_train)
+
+print("Best parameters for RandomForest: ", grid_rf.best_params_)
+print("Best estimator for RandomForest: ", grid_rf.best_estimator_)
+
+# KNeighbors com GridSearchCV
+param_grid_knn = {'n_neighbors': [3, 5, 7, 9, 11, 13, 15, 17, 19], 'weights': ['uniform', 'distance']}
+grid_knn = GridSearchCV(KNeighborsClassifier(), param_grid_knn, refit=True, verbose=3)
+grid_knn.fit(X_train, y_train)
+
+print("Best parameters for KNeighbors: ", grid_knn.best_params_)
+print("Best estimator for KNeighbors: ", grid_knn.best_estimator_)
+
+# Predição e avaliação com o melhor modelo de cada classificador
+classifiers = {
+    'SVM': grid_svm,
+    'RandomForest': grid_rf,
+    'KNeighbors': grid_knn
+}
+
+for name, clf in classifiers.items():
+    print(f"Results for {name} for dataset {baseFolder}:")
+    y_pred = clf.predict(X_test)
+    print(classification_report(y_test, y_pred))
+
+    # Matriz de confusão
+    cm = confusion_matrix(y_test, y_pred)
+    plt.figure(figsize=(10, 8))
+    sns.heatmap(cm, annot=True, fmt='d', cmap='Blues')
+    plt.title(f'Confusion Matrix for {name}')
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+    plt.savefig(f'confusion_matrix_{name}.png')
+    plt.close()
---
+++ minimaps.py
@@ -0,0 +1,155 @@
+from PIL import Image
+import os
+import hashlib
+import chardet
+
+caminho_base = 'D:/doutorado/nexus'
+# caminho_base = 'C:\\Users\\Nufinha\\work\\seeu\\src'
+caminho_saida = 'D:/doutorado/lbp_example-main/nexus_encrypted/'
+
+
+def encrypt_chars(asc_code):
+    if asc_code<33:        #não-imprimíveis
+        return 32
+    if asc_code<48:         #símbolos
+        return asc_code
+    if asc_code<58:         #números
+        return 53
+    if asc_code < 65:       #símbolos
+        return asc_code
+    if asc_code < 91:       #maiúsculas
+        return 77
+    if asc_code < 97:       #símbolos
+        return asc_code
+    if asc_code < 123:      #minúsculas
+        return 109
+    if asc_code < 127:       #símbolos
+        return asc_code
+    return 130
+
+def create_16_digit_hash(input_string):
+    input_bytes = input_string.encode('ISO-8859-1')
+    md5_hash = hashlib.md5(input_bytes).hexdigest()
+    hash_int = int(md5_hash, 16)
+    truncated_hash = hash_int % (10 ** 16)
+    hash_16_digit = f"{truncated_hash:016d}"
+    return hash_16_digit
+
+
+def list_files_recursive(path='.'):
+    for entry in os.listdir(path):
+        full_path = os.path.join(path, entry)
+        if os.path.isdir(full_path):
+            list_files_recursive(full_path)
+        else:
+            geraImagem(full_path)
+
+
+def geraImagem(caminho_arquivo):
+    try:
+        contador_linhas = 0
+        linha_mais_larga = 0
+
+        tipo = 'other'
+
+        if caminho_arquivo.endswith(".sql"):
+            tipo = 'sql'
+        elif caminho_arquivo.endswith("IT.java"):
+            tipo = 'javaintegrationtest'
+        elif caminho_arquivo.endswith("Test.java"):
+            tipo = 'javaunittest'
+        elif caminho_arquivo.endswith(".xhtml"):
+            tipo = 'jsf'
+        elif caminho_arquivo.endswith("DTO.java"):
+            tipo = 'javadto'
+        elif caminho_arquivo.endswith("TO.java"):
+            tipo = 'javato'
+        elif caminho_arquivo.endswith(".jrxml"):
+            tipo = "jasper"
+        elif caminho_arquivo.endswith("Builder.java"):
+            tipo = "javabuilder"
+        elif caminho_arquivo.endswith("Impl.java"):   # vai dar problema....
+            tipo = "javaimplementation"
+        elif caminho_arquivo.endswith(".sh"):
+            tipo = "sh"
+        elif caminho_arquivo.endswith(".yml"):
+            tipo = "yml"
+        elif caminho_arquivo.endswith(".json"):
+            tipo = "json"
+        elif caminho_arquivo.endswith(".xml"):
+            tipo = "xml"
+        elif caminho_arquivo.endswith(".properties"):
+            tipo = "properties"
+        elif caminho_arquivo.endswith("DataProvider.java"):
+            tipo = "javadataprovider"
+        elif caminho_arquivo.endswith(".svg"):
+            tipo = "svg"
+        elif caminho_arquivo.endswith("Configuration.java"):
+            tipo = "javaconfiguration"
+        elif caminho_arquivo.endswith(".js"):
+            tipo = "js"
+        elif caminho_arquivo.endswith(".css"):
+            tipo = "css"
+        elif caminho_arquivo.endswith("Converter.java"):
+            tipo = "javaconverter"
+
+
+        with open(caminho_arquivo, 'rb') as file:
+            raw_data = file.read()
+            result = chardet.detect(raw_data)
+            encoding = result['encoding']
+
+        with open(caminho_arquivo, 'r', encoding=encoding) as arquivo:
+            for linha in arquivo:
+                if '@Entity' in linha:
+                    tipo = 'entity'
+                elif '@Repository' in linha:
+                    tipo = 'repository'
+                elif '@Service' in linha:
+                    tipo = 'service'
+                elif '@Controller' in linha:
+                    tipo = 'controller'
+                elif '@RestController' in linha:
+                    tipo = 'restcontroller'
+                elif '@RestController' in linha:
+                    tipo = 'restcontroller'
+                elif '@Interface' in linha:
+                    tipo = 'annotation'
+                elif 'Interface' in linha:
+                    tipo = 'interface'
+
+
+                contador_linhas += 1  # Conta as linhas
+                linha_mais_larga = max(linha_mais_larga, len(linha.strip()))
+
+        if tipo == 'other':
+            return
+
+        largura, altura = linha_mais_larga, contador_linhas
+        imagem = Image.new('RGB', (largura, altura), 'black')
+        y = 0
+
+
+        with open(caminho_arquivo, 'r', encoding=encoding) as arquivo:
+            for linha in arquivo:
+                for x in range(len(linha.strip())):
+                    v = ord(linha[x])
+                    v = encrypt_chars(v)
+                    if v > 255:
+                        v = 255
+                    try:
+                        imagem.putpixel((x, y), (v, v, v))
+                    except IndexError:
+                        print(largura, altura, x, y, v)
+                y += 1
+
+        caminho_saida_com_tipo = caminho_saida + '/' + tipo + '/'
+        os.makedirs(caminho_saida_com_tipo, exist_ok=True)
+        arquivoImagem = create_16_digit_hash(caminho_arquivo) + '.png'
+        imagem.save(caminho_saida_com_tipo + arquivoImagem)
+
+    except Exception as ex:
+        print('ignorando', caminho_arquivo, ex)
+
+
+list_files_recursive(caminho_base)
